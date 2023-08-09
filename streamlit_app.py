a8f72bd2368f134ee9c463f88686ac558e6a1c18
jessicaqijiang <50718173+jessicaqijiang@users.noreply.github.com>
2023-08-08 22:08:01 -0700

    Update streamlit_app.py
    fix the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,12 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭Build Your own Fruit Smoothie🥝🍇')
 
 import pandas
-fruits_selected = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #put a pick list here so they can pick the frut they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selectd = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
