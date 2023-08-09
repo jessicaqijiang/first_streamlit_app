682b75f23c57b32e9560e8bc0dc1428a99c4da1d
jessicaqijiang <50718173+jessicaqijiang@users.noreply.github.com>
2023-08-08 22:03:28 -0700

    Update streamlit_app.py
    fix the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
